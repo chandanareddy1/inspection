fefd57036bdf1be42851b39d136a2cfdc5a4305c
chandanareddy1 <102997341+chandanareddy1@users.noreply.github.com>
2022-07-29 04:33:04 +0530

    Add files via upload

---
+++ Inspection/indexd.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Home</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+		<script src="assets/js/jquery-3.5.1.slim.min"></script>
+		<script src="bootstrap/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">    
+    <link rel="stylesheet" href="assets/css/style.css">    
+    <script src="assets/js/restrict.js"></script>
+  </head>
+  <body>
+  <body style="background-color:#ded6ff;">
+    <!-- including side navigations -->
+        <!-- header section -->        
+        <!-- header section end -->
+		<body>
+		
+    <nav class="navbar navbar-expand-sm bg-secondary navbar-dark">
+      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Inspection</a>
+      <input class="form-control form-control-dark w-50" type="text" placeholder="Search" aria-label="Search">
+      <ul class="navbar-nav px-3">
+        <li class="nav-item text-nowrap">
+          <a class="nav-link" href="index.php">Login</a>
+        </li>
+		<li class="nav-item text-nowrap">
+          <a class="nav-link" href="map.php">Maps</a>
+        </li>
+      </ul>
+    </nav>
+
+        <!-- form content -->
+        <!-- form content end --> 
+		<hr style="border-top: 2px solid #ffffff;">		
+		<!-- Gallery -->
+<div class="row">
+  <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+    <img
+      src="https://www.letsbuild.com/wp-content/uploads/2019/10/shutterstock_1247187910.png"
+      class="w-100 shadow-1-strong rounded mb-4"
+      alt="Boat on Calm Water"
+    />
+
+    <img
+      src="https://www.buildarray.com/assets/cms/uploads/images/construction_operations.jpg"
+      class="w-100 shadow-1-strong rounded mb-4"
+      alt="Wintry Mountain Landscape"
+    />
+  </div>
+
+  <div class="col-lg-4 mb-4 mb-lg-0">
+    <img
+      src="https://www.levelset.com/wp-content/uploads/2020/10/Fund-control-inspection-on-jobsite-e1601757995355.jpg"
+      class="w-100 shadow-1-strong rounded mb-4"
+      alt="Mountains in the Clouds"
+    />
+
+    <img
+      src="https://www.keyzo.co.uk/wp-content/uploads/2017/05/health-and-safety.jpg"
+      class="w-100 shadow-1-strong rounded mb-4"
+      alt="Boat on Calm Water"
+    />
+  </div>
+
+  <div class="col-lg-4 mb-4 mb-lg-0">
+    <img
+      src="https://5.imimg.com/data5/AN/BH/KW/SELLER-6373950/all-types-construction-workers-500x500.png"
+	  width="100" 
+     height="250"
+      class="w-100 shadow-1-strong rounded mb-4"
+      alt="Waves at Sea"
+    />
+
+    <img
+      src="https://www.constructionplusasia.com/wp-content/uploads/2020/06/lead-image-810x536.jpg"
+	  width="100" 
+	  height="235"
+      class="w-100 shadow-1-strong rounded mb-4"
+      alt="Yosemite National Park"
+    />
+  </div>
+</div>
+<!-- Gallery -->
+<!-- Footer -->
+<footer class="page-footer font-small cyan darken-3">
+
+  <!-- Footer Elements -->
+  <div class="container">
+
+    <!-- Grid row-->
+    <div class="row">
+
+      <!-- Grid column -->
+      <div class="col-md-12 py-4">
+        <div class="mb-1 flex-center">
+
+          <!-- Facebook -->
+          <a class="fb-ic">
+            <i class="fab fa-facebook-f fa-lg white-text mr-md-5 mr-5 fa-2x"> </i>
+          </a>
+          <!-- Twitter -->
+          <a class="tw-ic">
+            <i class="fab fa-twitter fa-lg white-text mr-md-5 mr-5 fa-2x"> </i>
+          </a>
+          <!-- Google +-->
+          <a class="gplus-ic">
+            <i class="fab fa-google-plus-g fa-lg white-text mr-md-5 mr-5 fa-2x"> </i>
+          </a>
+          <!--Linkedin -->
+          <a class="li-ic">
+            <i class="fab fa-linkedin-in fa-lg white-text mr-md-5 mr-5 fa-2x"> </i>
+          </a>
+          <!--Instagram-->
+          <a class="ins-ic">
+            <i class="fab fa-instagram fa-lg white-text mr-md-5 mr-5 fa-2x"> </i>
+          </a>
+          <!--Pinterest-->
+          <a class="pin-ic">
+            <i class="fab fa-pinterest fa-lg white-text fa-2x"> </i>
+          </a>
+        </div>
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row-->
+
+  </div>
+  <!-- Footer Elements -->
+  <!-- Copyright -->
+  <div class="footer-copyright text-center fa-lg py-3">© 2022 Copyright:
+    <a href="#" >Inspection</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+		
+      </div>
+    </div>
+  </body>
+</html>
---
+++ Inspection/map.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <head>
+    <meta charset="utf-8">
+    <title>Maps</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+		<script src="assets/js/jquery-3.5.1.slim.min"></script>
+		<script src="bootstrap/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">    
+    <link rel="stylesheet" href="assets/css/style.css">    
+    <script src="assets/js/restrict.js"></script>
+  </head>
+  <body>
+  <body style="background-color:#ded6ff;">
+    <!-- including side navigations -->
+        		<body>
+		
+    <nav class="navbar navbar-expand-sm bg-secondary navbar-dark">
+      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Inspection</a>
+      <input class="form-control form-control-dark w-50" type="text" placeholder="Search" aria-label="Search">
+      <ul class="navbar-nav px-3">
+        <li class="nav-item text-nowrap">
+          <a class="nav-link" href="index.php">Login</a>
+        </li>
+		<li class="nav-item text-nowrap">
+          <a class="nav-link" href="map.php">Maps</a>
+        </li>
+      </ul>
+    </nav>
+
+        <!-- form content -->
+		<div class="container-expand-sm p-3 my-3 bg-dark text-white">
+		<h1 class="text-center">UK MAPS</h1>
+		</div>
+ <div class="mapouter"><div class="gmap_canvas"><iframe width="1000" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=United%20kingdom&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://123movies-to.org"></a><br><style>.mapouter{position:relative;text-align:right;height:500px;width:1150px;}</style><a href="https://www.embedgooglemap.net"></a><style>.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:1150px;}</style></div></div>
+        <!-- form content end -->
+<!-- Footer -->
+<footer class="page-footer font-small cyan darken-3">
+
+  <!-- Footer Elements -->
+  <div class="container">
+
+    <!-- Grid row-->
+    <div class="row">
+
+      <!-- Grid column -->
+      <div class="col-md-12 py-4">
+        <div class="mb-1 flex-center">
+
+          <!-- Facebook -->
+          <a class="fb-ic">
+            <i class="fab fa-facebook-f fa-lg white-text mr-md-5 mr-5 fa-2x"> </i>
+          </a>
+          <!-- Twitter -->
+          <a class="tw-ic">
+            <i class="fab fa-twitter fa-lg white-text mr-md-5 mr-5 fa-2x"> </i>
+          </a>
+          <!-- Google +-->
+          <a class="gplus-ic">
+            <i class="fab fa-google-plus-g fa-lg white-text mr-md-5 mr-5 fa-2x"> </i>
+          </a>
+          <!--Linkedin -->
+          <a class="li-ic">
+            <i class="fab fa-linkedin-in fa-lg white-text mr-md-5 mr-5 fa-2x"> </i>
+          </a>
+          <!--Instagram-->
+          <a class="ins-ic">
+            <i class="fab fa-instagram fa-lg white-text mr-md-5 mr-5 fa-2x"> </i>
+          </a>
+          <!--Pinterest-->
+          <a class="pin-ic">
+            <i class="fab fa-pinterest fa-lg white-text fa-2x"> </i>
+          </a>
+        </div>
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row-->
+
+  </div>
+  <!-- Footer Elements -->
+  <!-- Copyright -->
+  <div class="footer-copyright text-center fa-lg py-3">© 2022 Copyright:
+    <a href="#" > Inspection</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->		
+      </div>
+    </div>
+  </body>
+</html>
